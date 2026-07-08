9ceff5eb40ef8157b2e1bb1216648036a8e310c7
WINDQI <2380570091.fcc@gmail.com>
2026-07-08 14:33:13 +0800

    fix: update logo to DSCODE pixel art

--- packages/console/app/src/routes/brand/index.tsx
+++ packages/console/app/src/routes/brand/index.tsx
@@ -5,18 +5,14 @@ import { Footer } from "~/component/footer"
 import { Legal } from "~/component/legal"
 import { useI18n } from "~/context/i18n"
 import { LocaleLinks } from "~/component/locale-links"
-import previewLogoLight from "../../asset/brand/preview-opencode-logo-light.png"
-import previewLogoDark from "../../asset/brand/preview-opencode-logo-dark.png"
-import previewLogoLightSquare from "../../asset/brand/preview-opencode-logo-light-square.png"
-import previewLogoDarkSquare from "../../asset/brand/preview-opencode-logo-dark-square.png"
+import previewLogoLight from "../../asset/brand/opencode-logo-light.svg"
+import previewLogoDark from "../../asset/brand/opencode-logo-dark.svg"
+import previewLogoLightSquare from "../../asset/brand/opencode-logo-light-square.svg"
+import previewLogoDarkSquare from "../../asset/brand/opencode-logo-dark-square.svg"
 import previewWordmarkLight from "../../asset/brand/preview-opencode-wordmark-light.png"
 import previewWordmarkDark from "../../asset/brand/preview-opencode-wordmark-dark.png"
 import previewWordmarkSimpleLight from "../../asset/brand/preview-opencode-wordmark-simple-light.png"
 import previewWordmarkSimpleDark from "../../asset/brand/preview-opencode-wordmark-simple-dark.png"
-import logoLightPng from "../../asset/brand/opencode-logo-light.png"
-import logoDarkPng from "../../asset/brand/opencode-logo-dark.png"
-import logoLightSquarePng from "../../asset/brand/opencode-logo-light-square.png"
-import logoDarkSquarePng from "../../asset/brand/opencode-logo-dark-square.png"
 import wordmarkLightPng from "../../asset/brand/opencode-wordmark-light.png"
 import wordmarkDarkPng from "../../asset/brand/opencode-wordmark-dark.png"
 import wordmarkSimpleLightPng from "../../asset/brand/opencode-wordmark-simple-light.png"
@@ -91,17 +87,6 @@ export default function Brand() {
               <div>
                 <img src={previewLogoLight} alt={alt} />
                 <div data-component="actions">
-                  <button onClick={() => downloadFile(logoLightPng, "opencode-logo-light.png")}>
-                    PNG
-                    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path
-                        d="M13.9583 10.6247L10 14.583L6.04167 10.6247M10 2.08301V13.958M16.25 17.9163H3.75"
-                        stroke="currentColor"
-                        stroke-width="1.5"
-                        stroke-linecap="square"
-                      />
-                    </svg>
-                  </button>
                   <button onClick={() => downloadFile(logoLightSvg, "opencode-logo-light.svg")}>
                     SVG
                     <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -118,17 +103,6 @@ export default function Brand() {
               <div>
                 <img src={previewLogoDark} alt={alt} />
                 <div data-component="actions">
-                  <button onClick={() => downloadFile(logoDarkPng, "opencode-logo-dark.png")}>
-                    PNG
-                    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path
-                        d="M13.9583 10.6247L10 14.583L6.04167 10.6247M10 2.08301V13.958M16.25 17.9163H3.75"
-                        stroke="currentColor"
-                        stroke-width="1.5"
-                        stroke-linecap="square"
-                      />
-                    </svg>
-                  </button>
                   <button onClick={() => downloadFile(logoDarkSvg, "opencode-logo-dark.svg")}>
                     SVG
                     <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -145,17 +119,6 @@ export default function Brand() {
               <div>
                 <img src={previewLogoLightSquare} alt={alt} />
                 <div data-component="actions">
-                  <button onClick={() => downloadFile(logoLightSquarePng, "opencode-logo-light-square.png")}>
-                    PNG
-                    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path
-                        d="M13.9583 10.6247L10 14.583L6.04167 10.6247M10 2.08301V13.958M16.25 17.9163H3.75"
-                        stroke="currentColor"
-                        stroke-width="1.5"
-                        stroke-linecap="square"
-                      />
-                    </svg>
-                  </button>
                   <button onClick={() => downloadFile(logoLightSquareSvg, "opencode-logo-light-square.svg")}>
                     SVG
                     <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -172,17 +135,6 @@ export default function Brand() {
               <div>
                 <img src={previewLogoDarkSquare} alt={alt} />
                 <div data-component="actions">
-                  <button onClick={() => downloadFile(logoDarkSquarePng, "opencode-logo-dark-square.png")}>
-                    PNG
-                    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path
-                        d="M13.9583 10.6247L10 14.583L6.04167 10.6247M10 2.08301V13.958M16.25 17.9163H3.75"
-                        stroke="currentColor"
-                        stroke-width="1.5"
-                        stroke-linecap="square"
-                      />
-                    </svg>
-                  </button>
                   <button onClick={() => downloadFile(logoDarkSquareSvg, "opencode-logo-dark-square.svg")}>
                     SVG
                     <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
